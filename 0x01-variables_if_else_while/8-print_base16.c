845255b5de96b5b759952f2840e500ab36589f8b
horace17 <hocedrix2010@gmail.com>
2022-07-01 19:44:48 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Great
+ * Return: end program
+ */
+
+int main(void)
+{
+	int a = '0';
+	int b = 'a';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar(10);
+	return (0);
+}
