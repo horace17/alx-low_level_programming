5c9b0e4d7be61ca4e2feee750e7e347f3e12e2fd
horace17 <hocedrix2010@gmail.com>
2022-07-05 14:12:38 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet - print the alphabet
- * Return: Always 0.
  */
 
 void print_alphabet(void)
@@ -11,8 +10,8 @@ void print_alphabet(void)
 	while (a <= 'z')
 	{
 		_putchar(a);
-		++a;									}
-	
+		++a;
+	}
 	_putchar(10);
 
 }
