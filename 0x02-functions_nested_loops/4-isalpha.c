7d79801edcd4bf9dffc457e5661a57ec65a03f43
horace17 <hocedrix2010@gmail.com>
2022-07-05 15:22:11 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,10 @@
 #include "main.h"
-
 /**
  * _isalpha - function to know if
-
  * the character is a letter or not
-
- *
-
  * @c: variable to evaluate
-
- *
-
  * Return: Get 1 if it's a letter,
-
  * 0 otherwise
-
  */
 
 int _isalpha(int c)
