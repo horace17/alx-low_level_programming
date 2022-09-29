091407c74bf915f68902dbeeb8d8b016c1927148
horace17 <hocedrix2010@gmail.com>
2022-09-29 12:54:31 -0700

    Task 0 corrected

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - count number of nodes
+ * @h: head of double linked list
+ * Return: number of nodes, or NULL if failed
+ *
+ */
+
 size_t print_dlistint(const dlistint_t *h);
 {
 	size_t n = 0;
